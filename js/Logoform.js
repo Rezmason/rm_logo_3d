f7bc689f60da744053e19fa35ac6dd6451340fd3
Rezmason <jeremysachs@rezmason.net>
2019-03-05 09:04:17 -0800

    The components of the logoform can now be laid out on a surface. This allows the volume to be 3D printed with fewer supports— but it ends up creating a large raft.

--- js/Logoform.js
+++ js/Logoform.js
@@ -1,6 +1,6 @@
 const makeLogoform = () => {
-    const geometries = [];
-    const makePusher = func => (...args) => geometries.push(func(...args));
+    const geometries = [[], [], [], [], [], [], []];
+    const makePusher = func => (i, ...args) => geometries[i].push(func(...args));
     const [addCurve, addStraight] = [makePusher(makeCurve), makePusher(makeStraight)];
 
     const whatWasIThinking = Math.SQRT1_2;
@@ -24,41 +24,115 @@ const makeLogoform = () => {
 
     const rSideRadius = zFront;
 
-    addCurve((yTop - yMiddle) / 2, 0, 1, xLeft + 2.25, (yTop + yMiddle) / 2 - thickness, zBase, 0, 0, -0.5); // R straightforward curl
-    addStraight((2.25 - rSideRadius - thickness), xLeft + rSideRadius + thickness, yTop - thickness, zBase, 0, 0.5); // R top left horizontal | straight bit
-    addStraight((2.25 - rSideRadius - thickness), xLeft + rSideRadius + thickness, yMiddle - thickness, zBase, 0, 0.5); // R bottom left horizontal | straight bit
-    addCurve(rSideRadius, 0.5, 1, xLeft + rSideRadius + thickness, yMiddle - thickness, zFront - thickness, 1.5); // R bottom left horizontal | bendy bit
-    addStraight(yMiddle - yBottom, xLeft + 0.5, yMiddle, zFront, 0.5, 0, 0); // R bottom left vertical
+    // R straightforward curl
+    addCurve(3,
+        (yTop - yMiddle) / 2, 0, 1,
+        /* POSITION */ xLeft + 2.25, (yTop + yMiddle) / 2 - thickness, zBase,
+        /* ROTATION */ 0, 0, -0.5
+    );
 
-    // • //addStraight(zFront - zBack - thickness * 2,  xRight - thickness, yBottom + thickness, zFront - thickness, 0, 1); // M bottom right base
-    addStraight(zFront - zBack - thickness * 2, xLeft + thickness, yBottom + thickness, zFront - thickness, 0, 1); // R bottom left base
+    // R top left horizontal | straight bit
+    addStraight(2,
+        (2.25 - rSideRadius - thickness),
+        /* POSITION */ xLeft + rSideRadius + thickness, yTop - thickness, zBase,
+        /* ROTATION */ 0, 0.5
+    );
 
-    addCurve(rRadius, 0, 1, xLeft + 1 + thickness + 0.25, (yMiddle + yBottom) / 2, 0, 0, 0.5, 0.5, t => {
-      const r1 = t * 3;
-      const r2 = t * 1.75;
-      return t * r2 + (1 - t) * r1;
-    }); // R helix
-    addCurve(mRadius, 1.5, 2.5, 0.1, 0, 0, 0, 0.5, 0, t => {
-      const r1 = t * 1.75;
-      const r2 = t * (width / 2 - Math.SQRT1_2 + 0.1);
-      return t * r2 + (1 - t) * r1;
-    }); // M helix
+    // R bottom left horizontal | straight bit
+    addStraight(4,
+        (2.25 - rSideRadius - thickness),
+        /* POSITION */ xLeft + rSideRadius + thickness, yMiddle - thickness, zBase,
+        /* ROTATION */ 0, 0.5
+    );
 
+    // R bottom left horizontal | bendy bit
+    addCurve(4,
+        rSideRadius, 0.5, 1,
+        /* POSITION */ xLeft + rSideRadius + thickness, yMiddle - thickness, zFront - thickness,
+        /* ROTATION */ 1.5
+    );
+
+    // R bottom left vertical
+    addStraight(0,
+        yMiddle - yBottom,
+        /* POSITION */ xLeft + 0.5, yMiddle, zFront,
+        /* ROTATION */ 0.5, 0, 0
+    );
+
+    // R bottom left base
+    addStraight(0,
+        zFront - zBack - thickness * 2,
+        /* POSITION */ xLeft + thickness, yBottom + thickness, zFront - thickness,
+        /* ROTATION */ 0, 1
+    );
+
+    // R helix
+    addCurve(5,
+        rRadius, 0, 1,
+        /* POSITION */ xLeft + 1 + thickness + 0.25, (yMiddle + yBottom) / 2, 0,
+        /* ROTATION */ 0, 0.5, 0.5,
+        t => {
+          const r1 = t * 3;
+          const r2 = t * 1.75;
+          return t * r2 + (1 - t) * r1;
+        }
+    );
+
+    // M helix
+    addCurve(6,
+        mRadius, 1.5, 2.5,
+        0.1, 0, 0,
+        0, 0.5, 0,
+        t => {
+          const r1 = t * 1.75;
+          const r2 = t * (width / 2 - Math.SQRT1_2 + 0.1);
+          return t * r2 + (1 - t) * r1;
+        }
+    );
 
     const mShootRadius = (zFront - zBack - thickness * 2) / 2;
-    addCurve(mShootRadius, 0, 1, xRight - thickness, yBottom + thickness * 1.5, zFront - mShootRadius, 0, 0.5, 0); // M hidden shoot | curvy bit
-    addStraight(thickness * 1.5, xRight - thickness, yBottom, zBack + thickness * 2, 1.5); // M hidden shoot | straight bit
 
-    // • //addStraight(height, xRight - thickness, height / 2, zFront, 0.5, 0, 0); // M right vertical
-    // • //addStraight(rRadius - thickness, xRight - thickness, height / 2 - thickness, 0, 0, 0); // M post-helix
+    // M hidden shoot | curvy bit
+    addCurve(1,
+        mShootRadius, 0, 1,
+        xRight - thickness, yBottom + thickness * 1.5, zFront - mShootRadius,
+        0, 0.5, 0
+    );
 
-    // • //addStraight(height, xRight - thickness, height / 2, zFront, 0.5, 0, 0); // M right vertical
+    // M hidden shoot | straight bit
+    addStraight(1,
+        thickness * 1.5,
+        xRight - thickness, yBottom, zBack + thickness * 2,
+        1.5
+    );
 
-    addCurve(rRadius, 0, 0.5, xRight - thickness, yTop - rRadius - thickness, 0, 0, 0.5, 0.5); // M right vertical | bendy bit
-    addStraight(height - rRadius - thickness, xRight - thickness, height / 2 - rRadius - thickness, zFront, 0.5, 0, 0); // M right vertical | straight bit
+    // M right vertical | bendy bit
+    addCurve(1,
+        rRadius, 0, 0.5,
+        xRight - thickness, yTop - rRadius - thickness, 0,
+        0, 0.5, 0.5
+    );
 
-    addCurve(rSideRadius, 0.5, 1, xLeft + rSideRadius + thickness, yTop - thickness, zBase - rSideRadius, 0.5); // R bottom left horizontal | bendy bit
-    addStraight((zBase - rSideRadius) - zBack - thickness, xLeft + thickness, yTop - thickness, zBase - rSideRadius, 0, 1); // R top left extension | curvy bit
+    // M right vertical | straight bit
+    addStraight(1,
+        height - rRadius - thickness,
+        xRight - thickness, height / 2 - rRadius - thickness, zFront,
+        0.5, 0, 0
+    );
 
-    return THREE.BufferGeometryUtils.mergeBufferGeometries(geometries);
+    // R bottom left horizontal | bendy bit
+    addCurve(2,
+        rSideRadius, 0.5, 1,
+        /* POSITION */ xLeft + rSideRadius + thickness, yTop - thickness, zBase - rSideRadius,
+        /* ROTATION */ 0.5
+    );
+
+    // R top left extension | curvy bit
+    addStraight(2,
+        (zBase - rSideRadius) - zBack - thickness,
+        /* POSITION */ xLeft + thickness, yTop - thickness, zBase - rSideRadius,
+        /* ROTATION */ 0, 1
+    );
+
+    return geometries;
 }
